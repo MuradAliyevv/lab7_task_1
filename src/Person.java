a1237fffde33884d86a43749db6eb6a86f9003d6
maliyev2 <maliyev2@stu.vistula.edu.pl>
2022-04-18 19:09:50 +0400

    Task is ready

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Person {
     private int id = 55400;
@@ -37,5 +40,102 @@ public class Person {
         System.out.println("Zip code: " + zipCode);
         System.out.println("City: " + city);
     }
+
+    interface Salary{
+        double salarytoPay(double hours, double rate);
+
+        default double salaryForOvertime(double overtimeHours, double rate){
+            return 1;
+        }
+
+        static int bonus(int yearsofExperience){
+            if (yearsofExperience>5){
+                return 100;
+            } else{
+                return 0;
+            }
+        }
+    }
+
+    public class Person{
+        String surname;
+        String name;
+        String street;
+        String zipCode;
+        String city;
+
+        public void initialize()throws IOException{
+            BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Data about a person");
+            System.out.println("Enter your surname: Aliyev");
+            try {
+                surname= br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Enter your name: ");
+            try {
+                name = br.readLine();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Enter the street: ");
+            street = br.readLine();
+            System.out.println("Enter your zip code: ");
+            zipCode= br.readLine();
+            System.out.println("Enter the city: ");
+            city = br.readLine();
+
+        }
+
+        public void print(){
+            System.out.println("Displaying the data:");
+            System.out.println("  ");
+            System.out.println("Surname: "+ surname);
+            System.out.println("Name: "+ name);
+            System.out.println("Street: "+street);
+            System.out.println("Zip-Code: "+zipCode);
+            System.out.println("City: "+ city);
+        }
+    }
+    static class Staff extends Person implements Salary {
+        String education;
+        String position;
+
+
+        public void initialize1() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            initialize();
+            System.out.println("Enter your education: ");
+            education = br.readLine();
+            System.out.println("Enter your position: ");
+            position = br.readLine();
+        }
+
+        public void print1() {
+            print();
+            System.out.println("Education: " + education);
+            System.out.println("Position: " + position);
+        }
+
+        @Override
+        public double salarytoPay(double hours, double rate) {
+            return hours * rate;
+        }
+
+
+    }
+    class HR{
+        public static void main(String[]args) throws IOException{
+            Staff employee=new Staff();
+
+
+            double hours;
+            double salary= employee.salarytoPay();
+
+
+        }
+    }
+
 
 }
--- src/Staff.java
+++ src/Staff.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Staff extends Person{
@@ -21,6 +24,37 @@ public class Staff extends Person{
         print();
         System.out.println("Education: " + education);
         System.out.println("Position: " + position);
+    }
+
+    public class staff extends Person implements Salary{
+        String education_Aliyev_55400;
+        String position_Aliyev_55400;
+
+        public void initialize1_Aliyev_55400() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            initialize1_Aliyev_55400();
+            System.out.println("Enter your education");
+            education_Aliyev_55400 = br.readLine();
+            System.out.println("Enter your position");
+            position_Aliyev_55400 = br.readLine();
+        }
+
+        public void print1_Aliyev_55400(){
+            print1_Aliyev_55400();
+            System.out.println("education:"+ education_Aliyev_55400);
+            System.out.println("position:"+ position_Aliyev_55400);
+        }
+
+        @Override
+        public void initialize() throws IOException {
+            super.initialize();
+        }
+
+        @Override
+        public double salarytoPay(double hours, double rate) {
+            return 0;
+        }
     }
 
 }
